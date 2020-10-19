c3a3b9d2739ea06740e4d5fc3f5c4d5c6ef6ad91
utkarshshobhari <59745179+utkarshshobhari@users.noreply.github.com>
2020-10-19 09:11:12 +0530

    Add files via upload

---
+++ build/precache-manifest.fc2bf2170a407fd7cf13fb20c03b9d6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23bf90ff9d6d486a6eaaa96bae60b1cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c85f30d5ed1447d5771",
+    "url": "/static/css/main.96b2a47e.chunk.css"
+  },
+  {
+    "revision": "0b3e5eb8d9d356a31205",
+    "url": "/static/js/2.0fac3181.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0fac3181.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c85f30d5ed1447d5771",
+    "url": "/static/js/main.de3fb5d8.chunk.js"
+  },
+  {
+    "revision": "8977261747f837f23154",
+    "url": "/static/js/runtime-main.5ae99878.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Sortingvisuals from './Sortingvisuals';
+function App() {
+  return (
+    <div className="App">
+      <Sortingvisuals></Sortingvisuals>
+    </div>
+  );
+}
+
+export default App;
